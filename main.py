f613395aca755b8c9391e3f642b59f70aa828f1d
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-24 10:35:41 -0400

    Add output window and buttons to GUI

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import scrolledtext
 
+def placeHolder():
+    pass
+
 # Create the main window
 window = tk.Tk()
 window.title("Toxic Comment Classifier")
@@ -10,8 +13,27 @@ window.geometry("800x600")
 inputLabel = tk.Label(window, text="Enter Comment: ")
 inputEntry = tk.Entry(window)
 
+outputText = scrolledtext.ScrolledText(window, width=90, height=20)
+
 # Configure the input label, input entry, and submit button to span the width of the window
-inputLabel.pack(side="left", anchor="n", padx=10, pady=10)
+inputLabel.pack(side="top", anchor="w", padx=10, pady=10)
 inputEntry.pack(side="top", fill="x", padx=10, pady=10)
+
+# Output window
+outputText.pack(side="top", fill="both", padx=10, pady=10)
+
+# Create the buttons
+barChartButton = tk.Button(window, text="Show Bar Chart", command=placeHolder) # Add function
+pieChartButton = tk.Button(window, text="Show Pie Chart", command=placeHolder) # Add function
+heatmapButton = tk.Button(window, text="Show Heatmap", command=placeHolder) # Add function
+
+# Position the buttons
+buttonFrame = tk.Frame(window)
+buttonFrame.pack(side="bottom", pady=10)
+barChartButton.pack(side="left", padx=(10, 5))
+pieChartButton.pack(side="left", padx=5)
+heatmapButton.pack(side="left", padx=(5, 10))
+
+
 
 window.mainloop()
