cfe6222262000b99de1446b8b2a768512beaa1d8
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-24 10:30:10 -0400

    Add input textbox and label

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ window = tk.Tk()
 window.title("Toxic Comment Classifier")
 window.geometry("800x600")
 
+# Create the text input box and submit button
+inputLabel = tk.Label(window, text="Enter Comment: ")
+inputEntry = tk.Entry(window)
+
+# Configure the input label, input entry, and submit button to span the width of the window
+inputLabel.pack(side="left", anchor="n", padx=10, pady=10)
+inputEntry.pack(side="top", fill="x", padx=10, pady=10)
+
 window.mainloop()
