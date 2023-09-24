b1f198d5af8accbcf504de596da2c62745267cfd
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-24 10:20:58 -0400

    Add main window GUI

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import scrolledtext
+
+# Create the main window
+window = tk.Tk()
+window.title("Toxic Comment Classifier")
+window.geometry("800x600")
+
+window.mainloop()
