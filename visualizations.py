43f9d6aea0387d8b9a83d6971de84e1dca1d4144
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-24 12:41:50 -0400

    Add heatmap to visualize the classification report

--- visualizations.py
+++ visualizations.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plot
 import numpy as np
 import data_handler
+import predictor
 
 # Shows a barchart of the toxic comment distribution by number.
 def create_bar_chart_number():
@@ -65,4 +66,88 @@ def create_bar_chart_categories():
     plot.xticks(x, categories, rotation=45)
 
     plot.tight_layout()
+    plot.show()
+
+# Shows a pie chart of the toxic comment distribution by percentages.
+def create_pie_chart():
+    # Define the toxic categories and their corresponding percentages
+    data = data_handler.get_csv_from_zip('toxic_subset_10901.zip', 'toxic_subset_10901.csv')
+    data_columns = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+    categories = ['Toxic', 'Non-toxic']
+    counts = [sum(data[column] == 1) for column in data_columns]
+
+    total_data = len(data)
+
+    # Total the number of comments that do not have toxic traits
+    toxic_comment_count = 0
+    for index, row in data.iterrows():
+        if any(row[column] == 1 for column in data_columns):
+            toxic_comment_count += 1
+
+    # Percentages of toxic and non-toxic comments
+    percent_toxic = toxic_comment_count / total_data
+    percent_non_toxic = 1 - percent_toxic
+
+    # Calculate the percentages for toxic comment distribution
+    percentages = [percent_toxic, percent_non_toxic]
+
+    # Create the pie chart
+    plot.figure()
+    plot.pie(percentages, labels=None)
+    plot.title('Toxic and Non-Toxic Comment\nDistribution by Percentage')
+
+    # Format the text for the legend
+    labels = [f'{category} ({(percentage * 100):.2f}%)' for category, percentage in zip(categories, percentages)]
+
+    # Adjust the legend, so it is not on top of the pie chart.
+    plot.legend(labels, loc='lower left', bbox_to_anchor=(0.86, 0.0))
+    plot.tight_layout()
+
+    plot.show()
+
+# Create a heatmap of the classification report data produced by sklearn metrics
+def createHeatMap():
+    report_data = predictor.get_classification_report()
+    # Get precision, recall, and F1-score data for each category
+    categories = list(report_data.keys())
+    metrics = ['precision', 'recall', 'f1-score']
+    data = np.zeros((len(categories), len(metrics)))
+
+    for i, category in enumerate(categories):
+        for j, metric in enumerate(metrics):
+            data[i, j] = report_data[category][metric]
+
+    # Create a heatmap using Matplotlib
+    figure, axis = plot.subplots()
+    heatmap = axis.imshow(data, cmap='coolwarm', vmin=0, vmax=1)
+
+    # Set the ticks on the value bar
+    axis.set_xticks(np.arange(len(metrics)))
+    axis.set_yticks(np.arange(len(categories)))
+
+    # Set the correct labels
+    label_y = ['Toxic', 'Severe Toxic', 'Obscene', 'Threat', 'Insult', 'Identity Hate', 'Micro Average', 'Macro Average',
+              'Weighted Average', 'Samples Average']
+    label_x = ['Precision', 'Recall', 'F1-Score']
+    axis.set_xticklabels(label_x)
+    axis.set_yticklabels(label_y)
+
+    # Rotate the tick labels and set alignment for better readability
+    plot.setp(axis.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    # Add colorbar
+    colorbar = plot.colorbar(heatmap)
+    colorbar.set_label('Value')
+
+    # Add values to the heatmap
+    for i in range(len(categories)):
+        for j in range(len(metrics)):
+            text = axis.text(j, i, f'{data[i, j]:.2f}', ha='center', va='center', color='black')
+
+    plot.title('Classification Report Heatmap')
+    plot.xlabel('Metrics')
+    plot.ylabel('Categories')
+    plot.tight_layout()
+    plot.subplots_adjust(bottom=0.18, top=0.94, right=0.75)
+
     plot.show()
