508a1b776ad2978c023f50cf58d93c5f27dceb25
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-24 12:22:59 -0400

    Add function to make a prediction

--- predictor.py
+++ predictor.py
@@ -1,5 +1,9 @@
 import joblib
+import pandas
+import data_handler
 import model_trainer
+
+from io import StringIO
 
 model = None
 vectorizer = None
@@ -26,3 +30,52 @@ def get_vectorizer():
         except FileNotFoundError:
             vectorizer = model_trainer.create_vectorizer()
     return vectorizer
+
+# Uses the model and vectorizer to make a prediction about whether a comment
+# would be considered toxic..
+def predict(comment):
+    # Formats the comment to be converted to a pandas data object, so it works
+    # correctly with the dataHandler.cleandata() function.
+    test_string = f'''comment_text
+    {comment}'''
+    ioStr = StringIO(test_string)
+    test_data = pandas.read_csv(ioStr)
+    cleaned_comment = data_handler.clean_data(test_data['comment_text'])
+
+    # Vectorize the cleaned comment.
+    vectorized_data = get_vectorizer().transform(cleaned_comment)
+
+    # Send the vectorized comment to the model to make a prediction.
+    prediction = get_model().predict(vectorized_data)
+
+    # Format the results of the prediction and return them.
+    result = prediction[0]
+    labels = ['toxic', 'severely toxic', 'obscene', 'threatening', 'insulting',
+              'identity hate']
+    count = sum(prediction[0])
+    response = f'The comment, "{comment}" is '
+    toxic_labels = []
+
+    for label, number in zip(labels, result):
+        if number == 1:
+            toxic_labels.append(label)
+
+    toxic_string = ""
+    if len(toxic_labels) == 1:
+        toxic_string = toxic_labels[0]
+    elif len(toxic_labels) == 2:
+        toxic_string = " and ".join(toxic_labels)
+    elif len(toxic_labels) > 2:
+        try:
+            toxic_string = ", ".join(toxic_labels[:-1])
+            + ", and "
+            + toxic_labels[-1]
+        except IndexError:
+            pass
+
+    if count == 0:
+        response += "non-toxic."
+    else:
+        response += toxic_string
+
+    return response
